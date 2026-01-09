61490e923116dd47686cd376fe00059f37ca8330
Will Sackfield <will.sackfield@gmail.com>
2026-01-09 12:41:07 +0800

    Export create_wt

--- panelbeater/__init__.py
+++ panelbeater/__init__.py
@@ -2,6 +2,7 @@
 
 from .download import download
 from .fit import fit
+from .wt import create_wt
 
-__VERSION__ = "0.2.2"
-__all__ = ["download", "fit"]
+__VERSION__ = "0.2.3"
+__all__ = ["download", "fit", "create_wt"]
